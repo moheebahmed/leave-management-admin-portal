69b798d3b226e0fa5d7d1e8a9f2dc872ad6fcace
Moheeb <moheebahmed606@gmail.com>
2026-03-26 20:52:58 +0500

    changes update

--- src/api/config.js
+++ src/api/config.js
@@ -3,5 +3,5 @@ export const API_BASE_URL = 'https://leave-management.conceptrecall.com/api'
 // export const API_BASE_URL = 'http://localhost:3000/api'
 
 export const getAuthHeaders = () => ({
-  Authorization: `Bearer ${localStorage.getItem('token')}`,
+  Authorization: `Bearer ${sessionStorage.getItem('token')}`,
 })
